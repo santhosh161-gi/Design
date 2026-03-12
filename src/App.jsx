e2a7c9958ffadfda9cbb4712aee8ff794a43e8ec
SANTHOSH MATHAVAN NT961 <santhoshmathavan.nt961@kpriet.ac.in>
2026-03-12 13:16:45 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import Collections from './animation/Collections'
 import Connect from './animation/Connect'
 import Knitting from './pages/Knitting'
 import FabricFinish from './pages/FabricFinish'
-import ConnectUs from './animation/ConnectUs'
+
 
 
 
@@ -74,8 +74,6 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/knitting" element={<Knitting />} />
         <Route path="/fabricfinish" element={<FabricFinish />} />
-        <Route path="/connect-us" element={<ConnectUs />} />
-
         {/* <Route path="/highqualitybamboocotton" element={<HighQualityBambooCotton />} />
         <Route path="/suvincotton" element={<SUVICotton />} />
         <Route path="/australiancotton" element={<AustralianCotton />} />
